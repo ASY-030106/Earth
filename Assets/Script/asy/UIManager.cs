bd8672f92acdbc8f55d498555883c96af7a67c46
ASY-030106 <asy951013@naver.com>
2023-09-17 18:43:22 +0900

    Tree 기본기능

--- Assets/Script/asy/UIManager.cs
+++ Assets/Script/asy/UIManager.cs
@@ -19,9 +19,8 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        //물이랑 비료 몇번 주면 자라게 할지는 추후에 결정
+        //물이랑 비료 몇번 주면 자라게 할지는 나무의 등급에 따라 결정
         WaterText.text = "물 " + TreeManager.Water + " / 5"; 
-        //횟수는 텍스트로 정하는 게 아니라 값을 받아오는 방식으로 수정
         ManureText.text = "비료 " + TreeManager.Manure + " / 5";
         if (TreeManager.Water >= 5)
         {
