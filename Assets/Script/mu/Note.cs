0794b2dc3b200633dd79e87d063328e309a6d284
snowplum212 <snowplum212@gmail.com>
2023-09-19 18:00:13 +0900

    b

--- Assets/Script/mu/Note.cs
+++ Assets/Script/mu/Note.cs
@@ -28,28 +28,28 @@ public class Note : MonoBehaviour
             float zValue= notePosition.z;
             // -1.15 -1.10 -1.05 -1.00 -0.95 -0.90 -0.85
             //  bad    gd    gr   pft    gr    gd   bad
-            if (zValue < -0.95f && zValue > -1.05)
+            if (zValue < -0.90f && zValue > -1.10)
             {
                 Debug.Log(zValue);
                 PlayerController.perfect++;
                 PlayerController.thisGold += 100;
                 PlayerController.instance.playerData.gold += 100;
             }
-            else if (zValue < -0.90f && zValue > -1.10)
+            else if (zValue < -0.85f && zValue > -1.15)
             {
                 Debug.Log(zValue);
                 PlayerController.great++;
                 PlayerController.thisGold += 50;
                 PlayerController.instance.playerData.gold += 50;
             }
-            else if (zValue < -0.85f && zValue > -1.15)
+            else if (zValue < -0.80f && zValue > -1.20)
             {
                 Debug.Log(zValue);
                 PlayerController.good++;
                 PlayerController.thisGold += 25;
                 PlayerController.instance.playerData.gold += 25;
             }
-            else if (zValue < -0.80f && zValue > -1.20)
+            else if (zValue < -0.75f && zValue > -1.25)
             {
                 Debug.Log(zValue);
                 PlayerController.bad++;
