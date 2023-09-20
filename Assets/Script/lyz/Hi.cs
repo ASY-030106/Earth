08813114d95afc718c77d95bfa7a59bff3d3117b
yxnjx <hnsky719719@gmail.com>
2023-09-20 21:37:33 +0900

    ㄱㄱ

--- Assets/Script/lyz/Hi.cs
+++ Assets/Script/lyz/Hi.cs
@@ -4,54 +4,48 @@ using UnityEngine;
 
 public class Hi : MonoBehaviour
 {
-    GameObject nearObject;
+    Rigidbody rigid;
+    public int trashCount;
+    public float jumpPower;
+    bool isJump;
 
     void Start()
     {
- 
+        rigid = GetComponent<Rigidbody>();
+        isJump = false;
     }
 
     void Update()
     {
-        //Input.GetAxis("Horizontal"); //수평 방향 입력시 -1에서 1까지 float return
-        //Input.GetAxisRaw //수평 방향 입력시 -1, 0, 1 3가지만 return
-        //Debug.Log(Input.GetAxis("Horizontal"));
-        //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        //Debug.Log(Input.GetAxisRaw("Vertical"));
-        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal")
-            , 0, Input.GetAxisRaw("Vertical"));
-        //vec => 다음 이동 위치의 벡터값 = 한 발자국
-        //Debug.Log(vec);
-
-        float speed = 0.03f;
-
-        //자기 포지션에서 계산된 Vector 값을 더해주면 됨
-        this.transform.position += (vec * speed);
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
     }
 
-    private void onTriggerStay(Collider other)
+    void FixedUpdate()
     {
-        if (other.name == "Trash")
-            Debug.Log("쓰레기와 닿았습니다.");
-            nearObject = other.gameObject;
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(h, 0, v);
+
+        rigid.AddForce(vec, ForceMode.Impulse);
     }
 
-    private void onTriggerExit(Collider other)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+            isJump = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Trash")
-            nearObject = null;
+        {
+            trashCount++;
+            other.gameObject.SetActive(false);
+            Debug.Log("주운 쓰레기 개수: " + trashCount);
+        }
     }
-
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Trash")
-        {
-            Debug.Log("쓰레기랑 충돌");
-            collision.gameObject.SetActive(false); //gameObject.SetActive(boolean) //오브젝트 비활성화 함수
-        }
-        else
-        {
-            Debug.Log("쓰레기 아닌 거랑 충돌");
-        }
-    }*/
 }
