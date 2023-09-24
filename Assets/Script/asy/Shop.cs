b30cf8cfbab9ff500d5dd366a4143f9edee52dc1
ASY-030106 <asy951013@naver.com>
2023-09-24 14:59:48 +0900

    Inventory

--- Assets/Script/asy/Shop.cs
+++ Assets/Script/asy/Shop.cs
@@ -10,6 +10,7 @@ public class Shop : MonoBehaviour
     public List<string> tree = new List<string>();
     public List<int> materialPrice = new List<int>();
     public List<string> material = new List<string>();
+    Dictionary<int, string> inventory;
     public GameObject[] treePanel;
     public GameObject[] meterialPanel;
     int i,minusPrice,number;
@@ -25,124 +26,133 @@ public class Shop : MonoBehaviour
         money.text = "돈 : " + GameManager.price;
         number = 0;
         isActive = true;
-    }
-
-    void Start()
-    {
-           
-    }
-
-    void Update()
-    {
-        
+        inventory = new Dictionary<int, string>();
     }
 
     public void ClickTree1()
     {
         Debug.Log("나무1 버튼 누름");
         BuyTree(0);
+        Inventory("나무1");
     }
 
     public void ClickTree2()
     {
         Debug.Log("나무2 버튼 누름");
         BuyTree(1);
+        Inventory("나무2");
     }
 
     public void ClickTree3()
     {
         Debug.Log("나무3 버튼 누름");
         BuyTree(2);
+        Inventory("나무3");
     }
 
     public void ClickTree4()
     {
         Debug.Log("나무4 버튼 누름");
         BuyTree(3);
+        Inventory("나무4");
     }
 
     public void ClickTree5()
     {
         Debug.Log("나무5 버튼 누름");
         BuyTree(4);
+        Inventory("나무5");
     }
 
     public void ClickTree6()
     {
         Debug.Log("나무6 버튼 누름");
         BuyTree(5);
+        Inventory("나무6");
     }
 
     public void ClickTree7()
     {
         Debug.Log("나무7 버튼 누름");
         BuyTree(6);
+        Inventory("나무7");
     }
 
     public void ClickTree8()
     {
         Debug.Log("나무8 버튼 누름");
         BuyTree(7);
+        Inventory("나무8");
     }
 
     public void ClickTree9()
     {
         Debug.Log("나무9 버튼 누름");
         BuyTree(8);
+        Inventory("나무9");
     }
 
     public void ClickMaterial1()
     {
         Debug.Log("재료1 버튼 누름");
         BuyMaterial(0);
+        Inventory("재료1");
     }
 
     public void ClickMaterial2()
     {
         Debug.Log("재료2 버튼 누름");
         BuyMaterial(1);
+        Inventory("재료2");
     }
 
     public void ClickMaterial3()
     {
         Debug.Log("재료3 버튼 누름");
         BuyMaterial(2);
+        Inventory("재료3");
     }
 
     public void ClickMaterial4()
     {
         Debug.Log("재료4 버튼 누름");
         BuyMaterial(3);
+        Inventory("재료4");
     }
 
     public void ClickMaterial5()
     {
         Debug.Log("재료5 버튼 누름");
         BuyMaterial(4);
+        Inventory("재료5");
     }
 
     public void ClickMaterial6()
     {
         Debug.Log("재료6 버튼 누름");
         BuyMaterial(5);
+        Inventory("재료6");
     }
 
     public void ClickMaterial7()
     {
         Debug.Log("재료7 버튼 누름");
         BuyMaterial(6);
+        Inventory("재료7");
     }
 
     public void ClickMaterial8()
     {
-        Debug.Log("재료2 버튼 누름");
+        Debug.Log("재료8 버튼 누름");
         BuyMaterial(7);
+        Inventory("재료8");
     }
 
     public void ClickMaterial9()
     {
-        Debug.Log("재료3 버튼 누름");
+        Debug.Log("재료9 버튼 누름");
         BuyMaterial(8);
+        Inventory("재료9");
     }
 
     public void BuyTree(int i)
@@ -258,7 +268,11 @@ public class Shop : MonoBehaviour
             {
                 left.gameObject.SetActive(false);
             }
-        }
-        
+        }      
+    }
+
+    public void Inventory(string slot)
+    {
+
     }
 }
