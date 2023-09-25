2d1e83849feb9283612928b2d11ef1ce1acda14f
ASY-030106 <asy951013@naver.com>
2023-09-25 21:35:06 +0900

    pull 전 commit

--- Assets/Script/asy/GameManager.cs
+++ Assets/Script/asy/GameManager.cs
@@ -6,7 +6,7 @@ public interface GameManager
 {
     static int Water { set; get; }
     static int Manure { set; get; }
-    static int WaterFinish { set; get; }
-    static int ManureFinish { set; get; }
+    //static int WaterFinish { set; get; } //줘야하는 물 몇번인지
+    //static int ManureFinish { set; get; } //줘야하는 비료 수
     static int price { set; get; } //지금 내가 가진 돈
 }
