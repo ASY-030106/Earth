aa2ca3ed042e4070a387aa5ed03956b2608bd293
ji0ne <zi0neeeeeeeee@gmail.com>
2023-11-05 20:59:30 +0900

    modify illigalCapture

--- Assets/Script/MiniGame/IllegalCapture/newIlligalCapture/Submarine.cs
+++ Assets/Script/MiniGame/IllegalCapture/newIlligalCapture/Submarine.cs
@@ -4,49 +4,61 @@ using UnityEngine;
 
 public class Submarine : MonoBehaviour
 {
-    public GameObject submarinePrefab;
-    public Vector3 surfaceSize = new Vector3(1f, 1f, 1f);
+    public GameObject submarinePrefab; 
+    //프리팹 오브젝트
+    public Vector3 surfaceSize , randomPosition;
+    //오브젝트 랜덤배치될 평면 사이즈 , 오브젝트가 랜덤으로 배치될 영역
 
     float timer, interval;
+
+    bool isSpawned;
+
+    int temp = 0;
 
     private void Awake()
     {
         interval = 1.0f;
+        surfaceSize = new Vector3(1f, 1f, 1f);
+        isSpawned = false;
+
     }
 
 
     void Start()
     {
-        Invoke("SpawnSubmarine", 0.5f);
+        StartCoroutine(SpawnSubmarineDelay());
+    } 
+    private IEnumerator SpawnSubmarineDelay()
+    {
+
+        while(true)
+        {
+            Debug.Log("interval 대기 직전");
+            yield return new WaitForSeconds(interval);
+
+            if (!isSpawned)
+            {
+                SpawnSubmarine();
+                temp++;
+                Debug.Log("스폰 : " + temp);
+                isSpawned = true;
+            }
+
+        }
+
     }
 
     public void SpawnSubmarine()
     {
-        Debug.Log("잠수함 스폰");
-
-        Vector3 randomPosition = new Vector3
-            (
-                Random.Range(-3f , 4f),             // x
-                Random.Range(-0.6f,-0.67f),         // y
-                Random.Range(-6f , -2f)             // z
-            );
+        randomPosition = new Vector3
+           (
+               Random.Range(-3, 4),            //x
+               Random.Range(-0.6f, -0.67f),    //y
+               Random.Range(-6, -2)            //z
+           );
 
         GameObject Submarine = Instantiate(submarinePrefab, randomPosition, Quaternion.identity);
+        isSpawned = false;
     }
-
-    //private void Update()
-    //{
-    //    timer += Time.deltaTime;
-
-    //    if(timer >= interval)
-    //    {
-    //        SpawnSubmarine();
-    //        timer = 0f;
-    //    }
-
-
-    //}
-
-
 
 }
